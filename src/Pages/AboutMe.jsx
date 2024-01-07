4cd7b3806f5b265349783cd81c5bb9848469b884
danielbrainich <143758962+danielbrainich@users.noreply.github.com>
2024-01-06 22:09:51 -0800

    more updates to formatting

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -4,32 +4,27 @@ export default function AboutMe() {
             <div className="container d-flex flex-column align-items-between justify-content-center">
                 <div className="row d-flex p-3">
                     <h2 className="section-title">About Me</h2>
-                    <p>
-                    Hi, I'm Daniel, a recent Hack Reactor bootcamp graduate and a full-stack software engineer. What excites me most about coding? Its limitless potential
-                    to solve problems, drive innovation, and create amazing things.
+                    <p className="mb-4">
+                    Hi, I'm Daniel, a Hack Reactor bootcamp graduate and a full-stack software engineer. At Hack Reactor, I fine-tuned my programming skills and built some fantastic projects, preparing myself for an exciting career in software development.
+                    But my coding journey began before than that.
                     <br></br>                    <br></br>
-                    At bootcamp, I fine-tuned my programming skills and built some fantastic projects, preparing myself for an exciting career in software development.
-                    However, my coding journey began a bit earlier than that.
+                    While completing an MA in International Affairs, I taught myself SQL to analyze and manipulate large datasets related to global development issues. This is
+                    where I first discovered that coding can be powerful, impactful, and also fun! My path from SQL to software engineering wasn't direct, though.
                     <br></br>                    <br></br>
-                    While completing an MA in International Affairs, I often relied on SQL to analyze and manipulate complex datasets related to global development issues. This is
-                    where I first discovered that coding can be powerful, impactful, and also fun!
-                    <br></br>                    <br></br>
-                    My path from SQL to software engineering wasn't direct, though. Before devoting myself full-time to coding, I first completed my Master's degree and then held
-                    a number of management roles in international development and the hospitality industry.
-                    <br></br>                    <br></br>
-                    My diverse academic and professional experience has been both challenging and rewarding. And now, I'm thrilled to bring this unique blend of skills and
-                    perspectives to the world of software engineering!
-
+                    Before devoting myself full-time to coding, I first completed my Master's degree and then held a number of management roles in the international development and
+                    hospitality industries. And now, I'm thrilled to bring my unique blend of skills and perspectives to the world of software engineering!
                     </p>
-                    <div className="card-body ps-0">
-                        <h5>languages</h5>
-                        <ul className="list-unstyled d-flex flex-column flex-md-row">
-                            <li className="me-md-3 mb-2 mb-md-0">Python</li>
-                            <li className="me-md-3 mb-2 mb-md-0">JavaScript</li>
-                            <li className="me-md-3 mb-2 mb-md-0">SQL</li>
-                            <li className="me-md-3 mb-2 mb-md-0">HTML</li>
-                            <li className="me-md-3 mb-2 mb-md-0">CSS</li>
-                        </ul>
+                    <div className="col-12 col-lg-4 my-2">
+                        <div className="card-body ps-0">
+                            <h5>languages</h5>
+                            <ul className="list-unstyled d-flex flex-column flex-md-row">
+                                <li className="me-md-3 mb-2 mb-md-0">Python</li>
+                                <li className="me-md-3 mb-2 mb-md-0">JavaScript</li>
+                                <li className="me-md-3 mb-2 mb-md-0">SQL</li>
+                                <li className="me-md-3 mb-2 mb-md-0">HTML</li>
+                                <li className="me-md-3 mb-2 mb-md-0">CSS</li>
+                            </ul>
+                        </div>
                     </div>
                     <div className="col-12 col-lg-4 my-2">
                         <div className="card-body">
@@ -48,6 +43,26 @@ export default function AboutMe() {
                                 <li className="me-md-3 mb-2 mb-md-0">React</li>
                                 <li className="me-md-3 mb-2 mb-md-0">Bootstrap</li>
                                 <li className="me-md-3 mb-2 mb-md-0">Tailwind</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-4 my-2">
+                        <div className="card-body">
+                            <h5>dev ops and deployment</h5>
+                            <ul className="list-unstyled d-flex flex-column flex-md-row">
+                                <li className="me-md-3 mb-2 mb-md-0">Docker</li>
+                                <li className="me-md-3 mb-2 mb-md-0">Heroku</li>
+                                <li className="me-md-3 mb-2 mb-md-0">Git</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-4 my-2">
+                        <div className="card-body">
+                            <h5>cms</h5>
+                            <ul className="list-unstyled d-flex flex-column flex-md-row">
+                                <li className="me-md-3 mb-2 mb-md-0">WordPress</li>
+                                <li className="me-md-3 mb-2 mb-md-0">Squarespace</li>
+                                <li className="me-md-3 mb-2 mb-md-0">Wix</li>
                             </ul>
                         </div>
                     </div>
